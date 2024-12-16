d0cf1282d313cdaa59c678883d57fdaae7a5075f
Mayur Dayal <mayur.dayal97@gmail.com>
2024-12-16 23:20:09 +0530

    feat(project): initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to My World!</h1>
+      <img src={reactLogo} className="logo react" alt="React logo" />
+    </>
+  );
+}
+
+export default App;
