8e5de5af6d7c00e643795c0b3ead57a4cc5cb0bb
Mayur Dayal <mayur.dayal97@gmail.com>
2024-12-17 14:53:26 +0530

    feat(css): integrate Tailwind CSS and PostCSS

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -12,14 +12,35 @@ function HomePage() {
 
   return (
     <>
-      <h1>Welcome to My World!</h1>
-      <ReactLogo className="logo react" />
+      <h1 className="text-center text-4xl my-5 mb-3">Welcome to My World!</h1>
+      <ReactLogo
+        className="mx-auto my-2 inline-block h-24 w-full transition-[filter] duration-300 will-change-[filter] hover:shadow-['#61dafbaa'] hover:drop-shadow motion-safe:animate-spin"
+        style={{ animationDuration: "10s" }}
+      />
 
-      <div>
-        <h3>Counter: {count}</h3>
-        <button onClick={() => dispatch(decremented())}>Decrement</button>
-        <button onClick={() => dispatch(incremented())}>Increment</button>
-        <button onClick={() => dispatch(amountAdded(3))}>Add 3</button>
+      <div className="my-24">
+        <h3 className="text-center text-2xl">Counter: {count}</h3>
+
+        <div className="flex items-center gap-x-2 justify-center my-2">
+          <button
+            onClick={() => dispatch(decremented())}
+            className="p-2 rounded-md bg-blue-600 hover:bg-blue-400 text-sm"
+          >
+            Decrement
+          </button>
+          <button
+            onClick={() => dispatch(amountAdded(3))}
+            className="p-2 rounded-md bg-purple-600 hover:bg-purple-500 text-sm"
+          >
+            Add 3
+          </button>
+          <button
+            onClick={() => dispatch(incremented())}
+            className="p-2 rounded-md bg-blue-600 hover:bg-blue-500 text-sm"
+          >
+            Increment
+          </button>
+        </div>
       </div>
     </>
   );
--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -2,10 +2,17 @@ import { Link } from "react-router";
 
 function NotFoundPage() {
   return (
-    <>
-      <h1>404 - This world was not found!</h1>
-      <Link to="/">Teleport Me</Link>
-    </>
+    <div className="flex items-center justify-center flex-col gap-y-4">
+      <h1 className="text-center text-4xl my-5 mb-3">
+        404 - This world was not found!
+      </h1>
+      <Link
+        to="/"
+        className="p-2 rounded-md bg-blue-600 hover:bg-blue-500 text-sm"
+      >
+        Teleport Me
+      </Link>
+    </div>
   );
 }
 
